8d48bd536c721d291ab2e4b1823aad7cf8855dc0
Gowthamraj Sekar <gowthamrajsekar31@gmail.com>
2025-03-06 03:32:05 +0530

    added feature 2

--- src/main/java/com/qa/opencart/pages/FeaturePage.java
+++ src/main/java/com/qa/opencart/pages/FeaturePage.java
@@ -5,6 +5,7 @@ public class FeaturePage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Feature 1");
+			System.out.println("Feature 2");
 	}
 
 }
